d76be840d6b9b4e652abcef01f5af2fc53681e32
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-03-13 15:05:16 -0700

    adding a js file with scrolling function

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//This function allows to aplly a scroll effect to any "a" that has an "id" as a target (internal link)
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+var target = this.hash;
+    $target = $(target);
+        $('html, body').stop().animate({'scrollTop': $target.offset().top}, 1500, 'swing', function () {
+        window.location.hash = target;
+        });
+    });
+});
