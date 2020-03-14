62bfaefb187ae67413fe920df439a1deda18987c
Mariana Caldas de Souza <marianacaldassouza@gmail.com>
2020-03-13 18:58:52 -0700

    adding the form section and footer

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //This function allows to aplly a scroll effect to any "a" that has an "id" as a target (internal link)
 $(document).ready(function(){
-    $('a[href^="#"]').on('click',function (e) {
+    $('a[href^="#"].nav-link').on('click',function (e) {
     e.preventDefault();
-var target = this.hash;
+    var target = this.hash;
     $target = $(target);
         $('html, body').stop().animate({'scrollTop': $target.offset().top}, 1500, 'swing', function () {
         window.location.hash = target;
